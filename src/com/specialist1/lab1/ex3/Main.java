f61605ee1708d707eaf536ebd1dd311971b44b14
D <daniil2705@gmail.com>
2021-02-18 10:05:36 +0300

    solution for ex3 in lab1

--- src/com/specialist1/lab1/ex3/Main.java
+++ src/com/specialist1/lab1/ex3/Main.java
@@ -3,6 +3,27 @@ package com.specialist1.lab1.ex3;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello3");
+        int m = 5;
+        System.out.println(getBinary(m));
+
+    }
+
+    private static String getBinary(int m) {
+        StringBuilder sb = new StringBuilder();
+        int i;
+        for(i=0; i<32; i++){
+            if((m&1)==1){
+                sb.append("1");
+            }
+            else{
+                sb.append("0");
+            }
+            m>>=1;
+            if(i==15 || i == 7 || i == 23){ sb.append(" "); }
+
+        }
+        sb.reverse();
+        return sb.toString();
+
     }
 }
